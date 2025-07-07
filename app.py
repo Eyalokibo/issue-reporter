bd2d3ea56f77a5b8babf8799ac64b63d213dd88a
Nir Ziv <nir@okibo.com>
2025-07-07 19:36:28 +0300

    fix file

--- app.py
+++ app.py
@@ -50,4 +50,5 @@ def get_issues():
     } for i in issues])
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+    port = int(os.environ.get("PORT", 5000))  # Render דורש שימוש ב־PORT מהסביבה
+    app.run(host='0.0.0.0', port=port)
