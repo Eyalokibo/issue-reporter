da1d3bd42f6daa32003c1384331b6a2dc66c9086
Nir Ziv <nir@okibo.com>
2025-07-07 20:24:20 +0300

    fix index

--- public/main.js
+++ public/main.js
@@ -2,7 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore, doc, setDoc } from "firebase/firestore";
 
-// 2. הגדרות Firebase שלך (העתק מה-Firebase Console שלך)
+// 2. הגדרות Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyAxvUtgrI5cOI9nYSh77vl8EkFtdWFxMXg",
   authDomain: "issuereporterbot.firebaseapp.com",
@@ -23,8 +23,10 @@ document.getElementById("issueForm").addEventListener("submit", async (e) => {
   const name = document.querySelector('input[name="name"]').value;
   const email = document.querySelector('input[name="email"]').value;
   const description = document.querySelector('textarea[name="description"]').value;
-
-  
+  const systemnumber = document.querySelector('input[name="systemnumber"]').value;
+  const failersolved = document.querySelector('input[name="failersolved"]').value;
+  const location = document.querySelector('input[name="location"]').value;
+  const date = document.querySelector('input[name="date"]').value;
 
   const customId = "issue-" + Date.now();
 
@@ -35,14 +37,14 @@ document.getElementById("issueForm").addEventListener("submit", async (e) => {
     systemnumber,
     failersolved,
     location,
-    date: new Date().toISOString()
+    date
   };
 
   try {
     await setDoc(doc(db, "issues", customId), issueData);
-    alert("Send!");
+    alert("Issue submitted successfully!");
   } catch (error) {
     console.error("error", error);
-    alert("reporting error ");
+    alert("Error submitting the issue.");
   }
 });
